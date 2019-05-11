8ac3384771da640f6b53e6d35c7e186c1bdc511c
Tiberiu Ioan <tiberiuioan35@gmail.com>
2019-05-11 21:38:43 +0300

    Initial commit

---
+++ js/buttons.js
@@ -0,0 +1,11 @@
+document.getElementById("playButton").onclick = function() {
+  location.href = "../game.html";
+};
+
+document.getElementById("optionsButton").onclick = function() {
+  location.href = "../options.html";
+};
+
+document.getElementById("exitButton").onclick = function() {
+  location.href = "../menu.html";
+};
---
+++ js/index.js
@@ -0,0 +1,5 @@
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d");
+
+const GAME_WIDTH = 1000;
+const GAME_HEIGHT = 800;

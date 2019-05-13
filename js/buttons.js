41c1a6aed95e74669628a8dafdaed92115ca6a08
Tiberiu Ioan <tiberiuioan35@gmail.com>
2019-05-13 20:47:31 +0300

    Added menu music + commented options tab

--- js/buttons.js
+++ js/buttons.js
@@ -2,9 +2,9 @@ document.getElementById("playButton").onclick = function() {
   location.href = "../game.html";
 };
 
-document.getElementById("optionsButton").onclick = function() {
-  location.href = "../options.html";
-};
+// document.getElementById("optionsButton").onclick = function() {
+//   location.href = "../options.html";
+// };
 
 document.getElementById("exitButton").onclick = function() {
   location.href = "../index.html";

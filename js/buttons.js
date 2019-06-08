4169bbd79f61e7a4ead0ed63023e746c008ad259
Tiberiu Ioan <tiberiuioan35@gmail.com>
2019-06-08 03:19:34 +0300

    Now it's possible to win

--- js/buttons.js
+++ js/buttons.js
@@ -1,13 +1,6 @@
 document.getElementById("playButton").onclick = function() {
   location.href = "../game.html";
 };
-document.getElementById("win").onclick = function() {
-  location.href = "../index.html";
-};
-
-// document.getElementById("optionsButton").onclick = function() {
-//   location.href = "../options.html";
-// };
 
 document.getElementById("exitButton").onclick = function() {
   location.href = "../index.html";
